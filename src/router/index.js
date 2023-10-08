0ded0b43d42e31f48e5689649743a9080771319a
QQBoxy <gs1458@gmail.com>
2023-10-09 02:10:02 +0800

    Router redirect

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,10 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/state=done',
+      redirect: '/'
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
