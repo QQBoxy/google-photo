d99edd18a5e73ec6faf201deb8e55ccd407ce32e
QQBoxy <gs1458@gmail.com>
2023-10-09 01:21:49 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  base: process.env.NODE_ENV === 'production'
+    ? '/google-photo/'
+    : '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
